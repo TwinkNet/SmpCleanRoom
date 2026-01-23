9382e2789ff3a5bca617b1ffb6491b1799475d87
TwinkNet <248537975+TwinkNet@users.noreply.github.com>
2026-01-22 19:36:23 -0700

    Initial commit

---
+++ src/main/java/network/twink/smpcleanroom/SmpCleanRoom.java
@@ -0,0 +1,16 @@
+package network.twink.smpcleanroom;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SmpCleanRoom extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
